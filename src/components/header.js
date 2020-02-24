50749c063742c843fac86e742d72e491de9aa7fd
Jin Park <jinpark.han@gmail.com>
2020-02-24 23:56:13 +0000

    Add header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,51 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+
+const Header = () => {
+
+  const data = useStaticQuery(graphql`
+           query {
+               site{
+                   siteMetadata {
+                       title
+                   }
+               }
+           }
+        `)
+
+  return (
+    <div className="text-center">
+      <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro|Open+Sans&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous"/>
+      <div className="mt-6">
+        <div className="mb-6">
+          <h1 className="text-6xl">{data.site.siteMetadata.title}</h1>
+          <span className="mx-1">Visualization</span>
+          <span className="mx-1">•</span>
+          <span className="mx-1">Full-Stack Developer</span>
+          <span className="mx-1">•</span>
+          <span className="mx-1">DevOps</span>
+        </div>
+        <div className="mb-6 text-xl">
+          <a className="mx-2" href='https://github.com/jin-park-dev'><i className="fab fa-github"></i></a>
+          <a className="mx-2" href='https://gitlab.jinis.online/jin'><i className="fab fa-gitlab"></i></a>
+          {/*<a href='https://twitter.com/codigoMate'><i className="fab fa-twitter"></i></a>*/}
+          {/*<a href='https://github.com/jin-park-dev'><i className="fab fa-dev"></i></a>*/}
+          <a className="mx-2" href='www.linkedin.com/in/jin-park-dev'><i className="fab fa-linkedin"></i></a>
+          {/*<a href='https://youtube.com/codigoMate'><i className="fab fa-youtube"></i></a>*/}
+        </div>
+        <div className="mb-6">
+          <Link className="mx-3" to="/">Portfolio</Link>
+          |
+          <Link className="mx-3" to="/about">About</Link>
+        </div>
+        <hr/>
+      </div>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header

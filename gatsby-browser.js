38500a1bed898d0cbae01f8ba7732c6f98bccb57
Jin Park <jinpark.han@gmail.com>
2020-02-24 21:47:53 +0000

    Initial commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+
+import "./src/css/tailwind_config.css"
+import "./src/css/global.css";
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <div className="mt-5">test tailwind</div>
   </Layout>
 )
 

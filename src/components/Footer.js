8e15de8b584acb837faa5e5c3e2f99b9cf189afb
Jin Park <jinpark.han@gmail.com>
2020-02-25 00:32:36 +0000

    Add footer. Add about page

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer p-3'>
+            <div>Jin Park © - 2020</div>
+            {/*© {new Date().getFullYear()}, Built with*/}
+            {/*{` `}*/}
+            {/*<a href="https://www.gatsbyjs.org">Gatsby</a>*/}
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>I am Developer</h1>
+    <p>These are a few of my projects. Open up for repo and live version!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <div className="mt-5">test tailwind</div>
   </Layout>
 )
 

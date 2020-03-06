be23e682d65ddfd750dc8c7b1fb08a3c08843352
Jin Park <jinpark.han@gmail.com>
2020-03-06 00:23:57 +0000

    Rewrite / Improve layout with tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,22 +11,21 @@ import './index.css';
 
 function renderFolio() {
   return  portfolioItems.map(({title, synopsis, idx, desc, repo, live,img}) => {
-    console.log(title);
     return (
-      <div key={'div' + idx} className="tab ">
-        <input key={'index' + idx}  type="checkbox" id={idx + title}/>
-        <label key={'label' + idx}  className="tab-label" for={idx + title}>
+      <div key={'div' + idx} className="flex flex-col">
+        {/*<div key={'index' + idx} id={idx + title}/>*/}
+        <div key={'label' + idx}  className="flex flex-col items-center justify-center sm:flex-row">
           {/*w-3/12 w-1/12 w-8/12*/}
-          <span key={'pTit' + idx} className='folio-title'>{title} </span>
-          <span key={'dot' + idx} className='folio-title '>· </span>
-          <span  key={'pSinop' + idx} className='folio-synopsis w-8/12'>{synopsis}</span>
-        </label>
-        <div key={'tab' + idx} class="tab-content">
-          <img alt="nicasia" src={img}/>
-          {desc}
-          <span className="repolinks" key={'repo' + idx}><a key={'repoLink' + idx} href={repo}>Github Repo</a></span>
-          {live ? <span className="repolinks" key={'live' + idx}><a key={'liveLink' + idx} href={repo}>Check it live</a></span> : null }
+          <div key={'pTit' + idx} className='font-bold text-xl sm:w-3/12'>{title}</div>
+          <div key={'dot' + idx} className='font-bold text-xl px-2'>·</div>
+          <div key={'pSinop' + idx} className='font-lg sm:w-9/12'>{synopsis}</div>
         </div>
+        {/*<div key={'tab' + idx} className="tab-content">*/}
+        {/*  <img alt="nicasia" src={img}/>*/}
+        {/*  {desc}*/}
+        {/*  <span className="repolinks" key={'repo' + idx}><a key={'repoLink' + idx} href={repo}>Github Repo</a></span>*/}
+        {/*  {live ? <span className="repolinks" key={'live' + idx}><a key={'liveLink' + idx} href={repo}>Check it live</a></span> : null }*/}
+        {/*</div>*/}
       </div>
     )
   })

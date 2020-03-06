833959facaea65d5ee963a3f437e6782f3e9a771
Jin Park <jinpark.han@gmail.com>
2020-03-06 08:41:03 +0000

    Add dropdown

---
+++ src/components/Portfolio.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Portfolio.css';
+
+const Portfolio = ({portfolioItem}) => {
+  const [hidden, setHidden] = useState(true);
+  const {title, synopsis, idx, desc, repo, live,img} = portfolioItem
+
+  return (
+    <div key={"div" + idx} className="flex flex-col mb-5"
+         onClick={() => setHidden(!hidden)}>
+      <div key={"label" + idx}
+           className="flex flex-col items-center justify-center sm:flex-row
+             cursor-pointer
+             transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+        <div key={"pTit" + idx} className='font-bold text-xl sm:w-3/12'>{title}</div>
+        <div key={"dot" + idx} className='font-bold text-xl px-2'>·</div>
+        <div key={"pSinop" + idx} className='font-lg sm:w-9/12'>{synopsis}</div>
+      </div>
+      <div key={"tab" + idx} className={`flex flex-row mt-6 ${hidden ? "twirl-section-collapsed" : "twirl-section"}`}>
+        <div className="w-4/12 mr-3"><img alt="nicasia" src={img}/></div>
+        <div className="flex flex-col w-8/12">
+          <div>{desc}</div>
+          <div className="" key={'repo' + idx}><a key={'repoLink' + idx} href={repo}>Github Repo</a></div>
+          {live ? <div className="" key={'live' + idx}><a key={'liveLink' + idx} href={repo}>Check it live</a></div> : null }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,13 @@ import SEO from "../components/seo"
 import portfolioItems from '../components/portfolio-items';
 
 import './index.css';
+import Portfolio from "../components/Portfolio"
 
 
 function renderFolio() {
-  return  portfolioItems.map(({title, synopsis, idx, desc, repo, live,img}) => {
+  return  portfolioItems.map((portfolioItem) => {
     return (
-      <div key={'div' + idx} className="flex flex-col">
-        {/*<div key={'index' + idx} id={idx + title}/>*/}
-        <div key={'label' + idx}  className="flex flex-col items-center justify-center sm:flex-row">
-          {/*w-3/12 w-1/12 w-8/12*/}
-          <div key={'pTit' + idx} className='font-bold text-xl sm:w-3/12'>{title}</div>
-          <div key={'dot' + idx} className='font-bold text-xl px-2'>·</div>
-          <div key={'pSinop' + idx} className='font-lg sm:w-9/12'>{synopsis}</div>
-        </div>
-        <div key={'tab' + idx} className="">
-          <div><img alt="nicasia" src={img}/></div>
-          <div>{desc}</div>
-          <span className="repolinks" key={'repo' + idx}><a key={'repoLink' + idx} href={repo}>Github Repo</a></span>
-          {live ? <span className="repolinks" key={'live' + idx}><a key={'liveLink' + idx} href={repo}>Check it live</a></span> : null }
-        </div>
-      </div>
+      <Portfolio portfolioItem={portfolioItem} />
     )
   })
 }

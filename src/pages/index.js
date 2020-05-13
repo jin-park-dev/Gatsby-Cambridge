06707f2b931ea31c7bf4ed682c09f91a825a8c0c
Jin Park <jinpark.han@gmail.com>
2020-05-13 15:38:10 +0100

    Minor word change

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,9 @@ function renderFolio() {
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>I am Developer</h1>
+    <h1>I am a Full-Stack Developer</h1>
     <h2>(Under construction...)</h2>
+    <p>I love learning, good discpline and being social</p>
     {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>*/}
     {/*  <Image />*/}
     {/*</div>*/}

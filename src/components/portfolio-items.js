7d164289770d97fc83c645976201bc212099a760
Jin Park <jinpark.han@gmail.com>
2020-06-21 00:24:51 +0100

    Add more portfolio

--- src/components/portfolio-items.js
+++ src/components/portfolio-items.js
@@ -1,27 +1,5 @@
 const portfolioItems = [
   {
-    title: 'KanXDoro',
-    synopsis: 'Kanban Board with pomodoro timer. Build with React/Redux, Django REST.',
-    desc: 'Custom Kanban Board with pomodoro timer. React/Redux, React-Beautiful-Dnd, Django REST, Django ORM, SQL.',
-    img: './portfolio/kanxdoro.png',
-    links: [
-      { "url": "https://www.kanxdoro.com", "content": "Link to KanXDoro" },
-      { "url": "https://github.com/jin-park-dev/React-LifeBoard", "content": "Link to Github" },
-    ],
-    idx: 1
-  },
-  {
-    title: 'Bachata Cambridge',
-    synopsis: 'Wagtail CMS (Based on Django) for longest running bachata performance group in UK.',
-    desc: 'Wagtail CMS (Based on Django) for longest running bachata performance group in UK - Bachata Obsesión Dance Team.',
-    img: './portfolio/bachata.png',
-    links: [
-      { "url": "https://www.bachatacambridge.com", "content": "Link to Bachata Cambridge" },
-      { "url": "https://github.com/jin-park-dev/bodt-cms", "content": "Link to Github" },
-    ],
-    idx: 2
-  },
-  {
     title: 'Cambridge - Winton Centre',
     synopsis: 'Wagtail CMS with custom calendar sync for University of Cambridge - Winton Centre for Risk and Evidence Communication.',
     desc: 'Wagtail CMS with custom calendar sync. Django, Bootstrap 4, SQL, Django ORM, SQL, API.',
@@ -29,18 +7,7 @@ const portfolioItems = [
     links: [
       { "url": "https://wintoncentre.maths.cam.ac.uk/", "content": "Link to Winton Centre" },
     ],
-    idx: 3
-  },
-  {
-    title: 'Fuzzy-Plotly',
-    synopsis: 'Open source uncertainty chart package with Python and Plotly.',
-    desc: '"Uncertainty is inherent to all scientific knowledge, and is vital to communicate in order to allow people to make decisions in light of the reliability of the evidence. However, people worry that communicating uncertainty might confuse, distract and undermine trust."',
-    img: './portfolio/fuzzy-plotly.png',
-    links: [
-      { "url": "https://wintoncentre.maths.cam.ac.uk/projects/communicating-uncertainty/", "content": "Link to Project Description" },
-      { "url": "https://github.com/WintonCentre/python-fuzzy-plots", "content": "Link to Github" },
-    ],
-    idx: 4
+    idx: 1
   },
   {
     title: 'RealRisk',
@@ -54,28 +21,52 @@ const portfolioItems = [
       { "url": "https://realrisk.wintoncentre.uk/", "content": "Link to RealRisk" },
       { "url": "https://url.wintoncentre.uk/SVWM0", "content": "Link to the Result on screenshot" },
     ],
+    idx: 2
+  },
+  {
+    title: 'KanXDoro',
+    synopsis: 'Kanban Board with pomodoro timer. Build with React/Redux, Django REST.',
+    desc: 'Custom Kanban Board with pomodoro timer. React/Redux, React-Beautiful-Dnd, Django REST, Django ORM, SQL.',
+    img: './portfolio/kanxdoro.png',
+    links: [
+      { "url": "https://www.kanxdoro.com", "content": "Link to KanXDoro" },
+      { "url": "https://github.com/jin-park-dev/React-LifeBoard", "content": "Link to Github" },
+    ],
+    idx: 3
+  },
+  {
+    title: 'Bachata Cambridge',
+    synopsis: 'Wagtail CMS (Based on Django) for longest running bachata performance group in UK.',
+    desc: 'Wagtail CMS (Based on Django) for longest running bachata performance group in UK - Bachata Obsesión Dance Team.',
+    img: './portfolio/bachata.png',
+    links: [
+      { "url": "https://www.bachatacambridge.com", "content": "Link to Bachata Cambridge" },
+      { "url": "https://github.com/jin-park-dev/bodt-cms", "content": "Link to Github" },
+    ],
+    idx: 4
+  },
+  {
+    title: 'Fuzzy Plotly',
+    synopsis: 'Open source uncertainty chart package with Python and Plotly.',
+    desc: '"Uncertainty is inherent to all scientific knowledge, and is vital to communicate in order to allow people to make decisions in light of the reliability of the evidence. However, people worry that communicating uncertainty might confuse, distract and undermine trust."',
+    img: './portfolio/fuzzy-plotly.png',
+    links: [
+      { "url": "https://wintoncentre.maths.cam.ac.uk/projects/communicating-uncertainty/", "content": "Link to Project Description" },
+      { "url": "https://github.com/WintonCentre/python-fuzzy-plots", "content": "Link to Github" },
+    ],
     idx: 5
   },
-
-  // {
-  //     title: 'Portfolio Item 4',
-  //     synopsis: 'Its very minimalist, but that was the end goal',
-  //     desc: 'This was inspired in Dinesh Pandiyan personal page',
-  //     img: './portfolio/EJS.png',
-  //     repo: 'https://github.com/martin2844',
-  //     live: 'https://my-ejs-todo-list.herokuapp.com',
-  //     idx: 4
-  // },
-  // {
-  //     title: 'Portfolio Item 5',
-  //     synopsis: 'Please message me at twitter @codigoMate',
-  //     desc: 'I really appriciate any messages',
-  //     img: './portfolio/Mate.png',
-  //     repo: 'https://github.com/martin2844',
-  //     live: 'https://codigomate.com',
-  //     idx: 5
-  // }
-
+  {
+    title: 'PWA Pomodoro',
+    synopsis: 'Open source installable pomodoro app (PWA) built with ClojureScript',
+    desc: 'PWA-Pomodoro is progressive web app installable on Android, iOS, Desktop. Built with ClojureScript, Reagent, Re-frame, Google Workbox, Tailwind CSS.',
+    img: './portfolio/pwa-pomodoro-pomo-iphone.png',
+    links: [
+      { "url": "https://pomo.kanxdoro.com/", "content": "Link to PWA-Pomodoro" },
+      { "url": "https://gitlab.jinis.online/jin/pomotimer-plusplus", "content": "Link to Gitlab" },
+    ],
+    idx: 6
+  },
 ]
 
 export default portfolioItems
--- src/pages/about.js
+++ src/pages/about.js
@@ -33,7 +33,9 @@ export const query = graphql`
 const About = (props) => {
   return (
     <Layout>
-      <h1 className="text-center sm:text-left mb-3 sm:mb-6">Hello, my name is Jin</h1>
+      <h1 className="text-center sm:text-left mb-3 sm:mb-6">
+        Hello, my name is Jin
+      </h1>
 
       <div className="flex flex-col sm:flex-row text-sm items-center sm:items-start">
         <div className="w-12/12 sm:w-4/12">
@@ -66,63 +68,96 @@ const About = (props) => {
             </div>
           </div>
         </div>
-        
+
         <div className="w-12/12 sm:w-8/12 text-justify sm:text-justify px-1">
-          <hr className="my-5 sm:hidden"/> {/* Mobile only */}
+          <hr className="my-5 sm:hidden" /> {/* Mobile only */}
           <div>
             <p>
-              Technology enthusiast. Since young age I been interested in impact
-              technology had on our lives.
+              Technology enthusiast. Since young age I been interested in
+              impact, technologies have on our lives.
             </p>
             <p>
               My core skillset is building web applications from start to finish
-              throughout development lifecycle. Commonly referred as a Full-Stack
-              developer.
+              throughout development lifecycle. Commonly referred as a
+              Full-Stack developer.
             </p>
             <p>
-              General competency in DevOPs area. Cloud services such
-              as Digital Ocean, GCP, Proxmox, virtual machines, CI, docker,
-              ansible, bash, zsh. Toolsets to facilitate in development,
-              deployment and maintenance of a software.
+              General competency in DevOPs area. Cloud services such as Digital
+              Ocean, GCP, Proxmox, virtual machines, CI, docker, ansible, bash,
+              zsh. Toolsets to facilitate in development, deployment and
+              maintenance of a software.
             </p>
             <p>
               I focus on balancing delivering best value to business whilst
-              keeping high standard of software development. This must be decided
-              on case by case and correct balance must be applied for the
-              situation.
+              keeping high standard of software development. On case by case and
+              correct balance must be applied for the situation.
             </p>
             <p>
-              Software developer is a craft and requires continuously honing
-              the craft. In modern world of fast moving tech space it is important
-              to continuously learn and adapt as required.
+              Software developer is a craft and requiring continuously honing
+              the craft. In modern world of fast moving tech space it is
+              important to continuously learn and adapt as required.
             </p>
             <p>
-              In my spare time I enjoy exploring new technology and practising my
-              craft.
+              In my spare time I enjoy exploring new technology and practising
+              my craft.
             </p>
             <p className="mb-0">
-              I am also a dancer. I teach, perform and socially dance. Being in the moment, going along with whatever happens gives break to my analytical side of brain.
+              I am also a dancer. I teach, perform and socially dance. Being in
+              the moment, going along with whatever happens gives break to my
+              analytical side of brain.
             </p>
           </div>
-
           <div className="flex flex-col items-center mt-5">
-            <p>
-              Some of the tech I like:
-            </p>
+            <p>Some of the tech I like:</p>
             {/* self-center fixes safari bug */}
             <div className="flex flex-row flex-wrap justify-center mt-2 sm:mt-0">
-              <img alt="Python" className="w-8 mx-4 self-center" src={pythonVertical2Img} />
-              <img alt="Javascript" className="w-8 mx-4 self-center" src={javascriptImg} />
-              <img alt="Clojure" className="w-8 mx-4 self-center" src={clojureImg} />
-              <img alt="ClojureScript" className="w-8 mx-4 self-center" src={cljsWhiteImg} />
-              <img alt="React" className="w-8 mx-4 self-center" src={reactImg} />
-              <img alt="Django" className="w-8 mx-4 self-center" src={djangoImg} />
-              <img alt="Wagtail-CMS" className="w-8 mx-4 self-center" src={wagtailImg} />
-              <img alt="TailwindCss" className="w-8 mx-4 self-center" src={tailwindcssImg} />
-              <img alt="Gatsby" className="w-8 mx-4 self-center" src={gatsbyImg} />
+              <img
+                alt="Python"
+                className="w-8 mx-4 self-center"
+                src={pythonVertical2Img}
+              />
+              <img
+                alt="Javascript"
+                className="w-8 mx-4 self-center"
+                src={javascriptImg}
+              />
+              <img
+                alt="Clojure"
+                className="w-8 mx-4 self-center"
+                src={clojureImg}
+              />
+              <img
+                alt="ClojureScript"
+                className="w-8 mx-4 self-center"
+                src={cljsWhiteImg}
+              />
+              <img
+                alt="React"
+                className="w-8 mx-4 self-center"
+                src={reactImg}
+              />
+              <img
+                alt="Django"
+                className="w-8 mx-4 self-center"
+                src={djangoImg}
+              />
+              <img
+                alt="Wagtail-CMS"
+                className="w-8 mx-4 self-center"
+                src={wagtailImg}
+              />
+              <img
+                alt="TailwindCss"
+                className="w-8 mx-4 self-center"
+                src={tailwindcssImg}
+              />
+              <img
+                alt="Gatsby"
+                className="w-8 mx-4 self-center"
+                src={gatsbyImg}
+              />
             </div>
           </div>
-
           <div className="flex flex-row justify-center">
             <div className="mr-3">Send me a message!</div>
             <a className="mr-2" href="https://github.com/jin-park-dev">
@@ -131,10 +166,7 @@ const About = (props) => {
             <a className="mr-2" href="https://www.linkedin.com/in/jin-park-dev">
               <i className="fab fa-linkedin"></i>
             </a>
-            <a
-              className="mr-2"
-              href="https://www.instagram.com/jinnotonik/"
-            >
+            <a className="mr-2" href="https://www.instagram.com/jinnotonik/">
               <i className="fab fa-instagram"></i>
             </a>
           </div>

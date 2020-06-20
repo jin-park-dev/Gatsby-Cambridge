ab921e20d13cf53260e17c98ecd4e3848d8ae846
Jin Park <jinpark.han@gmail.com>
2020-06-21 00:45:32 +0100

    Clean up build, publish script.

--- build_and_surge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "Clean, Building static files to be uploaded"
-echo ""
-gatsby clean
-gatsby build
-echo "Uploading to surge.sh"
-surge public/ --domain jin-dev.surge.sh
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gatsby clean && gatsby develop
